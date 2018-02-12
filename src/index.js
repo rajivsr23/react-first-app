f90efe2e75f7580a820e8d0db477d7d2e67b9dc0
Ricky Raghavan <rajivsr@yahoo.com>
2018-02-11 23:34:18 -0500

    Replaced Square Class with a Function

--- src/index.js
+++ src/index.js
@@ -1,50 +1,31 @@
-//Tic Tac Toe
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-class Square extends React.Component {
-
-//Adding a Constructor to the class in order to Initialize the state
-constructor(props){
-super(props);
-this.state={
-value: null,
-
-};
-
-
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
-  render() {
-    return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
-      </button>
-    );
-  }
-}
-
 
 class Board extends React.Component {
-
-  constructor(props){
-super(props);
-this.state={
-squares: Array(9).fill(null),
-
-};
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
   }
-  handleClick(i){
-const squares=this.state.squares.slice();
-squares[i]='X';
-this.setState({squares: squares});
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
   }
+
   renderSquare(i) {
-    return <Square  value={this.state.squares[i]}
-    onClick={() => this.handleClick(i)}/>;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
     );
   }
 
@@ -96,5 +77,3 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
-//End
-
