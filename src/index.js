17635dec0027c102980a0805c4eefc237082df12
Ricky Raghavan <rajivsr@yahoo.com>
2018-03-18 11:07:10 -0400

    Added Comments

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ xIsNext: !this.state.xIsNext,
   }
 //Render Method
 
-//Modified-Method-26-In Progress...
+//Modified-Method-27-In Progress...
   render() {
     const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
